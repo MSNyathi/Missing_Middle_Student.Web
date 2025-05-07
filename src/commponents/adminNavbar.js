fe92c4a1c3f24c0e29269aab74b4296f3b875d02
Xolane Shabalala <143074915+Xolane3@users.noreply.github.com>
2025-05-07 15:18:50 +0200

    a

--- src/commponents/adminNavbar.js
+++ src/commponents/adminNavbar.js
@@ -5,7 +5,7 @@ import './adminNavbar.css';
 const AdminNavbar = () => {
   return (
     <div className="d-flex flex-column vh-100 p-3 bg-primary text-white" style={{ width: '220px' }}>
-      <h2 className="mb-4">📘 eduConnect</h2>
+      <h2 className="mb-4">💻 eduConnect</h2>
       <ul className="nav nav-pills flex-column">
         <li className="nav-item">
           <Link to="/admin/dashboard" className="nav-link text-white">🏠 Home</Link>
