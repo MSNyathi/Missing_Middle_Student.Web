a6029dc636181c56096807e458bc97d55b2f0b11
DrippyJayy67 <letswalolethabo449@gmail.com>
2025-05-09 09:42:10 +0200

    updated landing page and login page

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -11,7 +11,9 @@ function Login() {
             
             <div className="App">
             <nav id="mynav">
+                <Link to="/">
                 <img id="myimg" src={tut25} classname="bcolor"/>
+                </Link>
                 <h1 id="myh1">EduConnect</h1>
                 <button id="btn"><i id="i" className="bi bi-person-circle" style={{ fontSize: '1.5rem' } }></i>SWITCH USER</button>
             </nav>
@@ -20,8 +22,8 @@ function Login() {
             <i className="bi bi-person-circle" id="i1" style={{ fontSize: '4rem' }}></i>
                 <h1 id="myh1">Student Login</h1>
                 <p id="p1">Welcome back! Please enter your login details below.</p>
-                <form>
-                    <table>
+                <form id="myform1">
+                    <table id="tbl">
                         <tr>
                             <td id="td1">
                                 <label htmlFor="username">Username:</label>
@@ -29,7 +31,7 @@ function Login() {
                         </tr>
                         <tr>
                             <td>
-                                <input type="text" id="username" name="username" required />
+                                <input type="text" id="input1" name="username" required />
                             </td>
                         </tr>
                         <tr>
@@ -39,15 +41,14 @@ function Login() {
                         </tr>
                         <tr>
                             <td>
-                                <input type="password" id="password" name="password" required />
+                                <input type="password" id="input1" name="password" required />
                             </td>
                         </tr>
-                        <tr>
-                            <td>
-                                <Link to="/student">
-                            <button type="submit" id="btn">Login</button>
+                        <tr id="trbtn">
+                        <Link to="/student">
+                            <button type="submit" id="btn2">Login</button>
                             </Link>
-                            </td>
+                            
                         </tr>
                     </table>
                     
