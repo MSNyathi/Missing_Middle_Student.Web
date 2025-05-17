550cf4bee430c70e62c203e188c37970f0aff1fc
Xolane Shabalala <143074915+Xolane3@users.noreply.github.com>
2025-05-17 11:17:06 +0200

    use client added

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import AdminNavbar from "../../../commponents/adminNavbar";
 import { Bar, Pie, Line } from "react-chartjs-2";
--- src/pages/admin/register/index.js
+++ src/pages/admin/register/index.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { Image, Modal, Button, Form } from 'react-bootstrap';
 import { motion } from 'framer-motion';
