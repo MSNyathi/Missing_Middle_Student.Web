7660ad98d2e1b66956889540cbef0244398e581c
DrippyJayy67 <letswalolethabo449@gmail.com>
2025-05-29 21:16:05 +0200

    update
    landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@
 import tut25 from './assets/tut25.png'; // Adjust the path to your image
 //import './App.css';
 import React from 'react';
+import img1 from './assets/397758.jpg';
+import './LandingPage2.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Link } from 'react-router-dom';
 //import backgroundImage from '../src/assets/backgroundAdmin.jpeg'; // Adjust path if needed
@@ -32,7 +34,67 @@ function App() {
       <h1 id="myh1">EduConnect</h1>
       
     </nav>
+    {/*=============================================*/} 
+
     
+      <section id="lpsection1">
+        
+        <div>
+          <div>
+            <h2 id="myh1">Welcome to EduConnect</h2>
+            <h2 id="p1">Empowering Students Through Technology</h2>
+            <div>
+              <p id="p1">In today's digital age, access to technology is essential for academic success. </p>
+              <p id="p1">Recognizing this need, our initiative is dedicated to providing refurbished laptops to deserving students within Tshwane University of Technology. </p>
+              <p id="p1">By donating your old or unused laptops, you can help bridge the digital divide and empower the next generation of leaders.</p>
+        
+            </div>
+            
+          </div>
+          
+        
+        </div>
+      </section>
+      <section id="lpsection2">
+        <div id="lpsection2div1">
+          <div id="lpsection2div1div1">
+            <div id="lpsection2div1div1div1">
+              <h3>Login</h3>
+            </div>
+            
+          </div>
+        </div>
+        <div id="lpsection2div2">
+          <div id="lpsection2div1div1">
+            <div id="lpsection2div1div1div1">
+              <h3>Announcements</h3>
+            </div>
+            
+          </div>
+        </div>
+        <div id="lpsection2div3">
+          <div id="lpsection2div1div1">
+            <div id="lpsection2div1div1div1">
+              <h3>Application Requirements</h3>
+            </div>
+            
+          </div>
+        </div>
+        <div id="lpsection2div4">
+          <div id="lpsection2div1div1">
+            <div id="lpsection2div1div1div1">
+              <h3>Success Stories</h3>
+            </div>
+            
+          </div>
+        </div>
+        
+        
+        
+        
+      </section>
+    
+    {/*=============================================*/} 
     <section id="landingsection2" class="py-12 px-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6 text-center">
     <h2 id="p1">Welcome</h2>
     <p id="p1" class="max-w-2xl mx-auto text-lg">Serving Donors, Supervisors, Admins, Students, and Technicians to streamline collaboration and support.</p>
