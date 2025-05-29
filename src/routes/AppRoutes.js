805508c18a059125e3f4215be8b3c6816e9b6952
tsetuke1 <tsetuke@gmail.com>
2025-05-29 11:39:52 +0200

    Updated the apply for laptop page via navbar and fixed the forgot password route for student
    Updated the apply for laptop page via navbar and fixed the forgot password route for student

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,27 +6,21 @@ import Applicants from '../pages/admin/applicants';
 import RegisterTechnician from '../pages/admin/register'; // registration page
 import App from '../App'; // default landing component
 import AssignDevicePage from '../pages/admin/devices/AssignDevices/assignDevice'
-import StudentDashboard from '../pages/student/dashboard';
+import StudentDashboard from '../pages/student/dashboard/index';
 import ApplyLaptop from '../pages/student/applyLaptop';
 import TrackApplication from '../pages/student/trackApplication';
-import Login from '../pages/student/login/studentLogin'
+import HelpPageLaptops from '../pages/student/help/helpPage'; // help page
 import RegisterForm from '../pages/student/register/RegisterForm';
 import SuccessPage from '../pages/student/register/SuccessPage';
 import AdminLogin from '../pages/admin/login/adminLogin';
 import ViewDevices from '../pages/admin/devices/viewDevices/viewDevice';
 import ForgotPassword from '../pages/admin/login/forgotPassword'; // forgot password page
+import StudentForgotPassword from '../pages/student/login/studentforgotPassword'; // forgot password page
 import Students from '../pages/admin/students/students';
 import TechnicianDashboard from '../pages/Technician/TechnicianDashboard';
-import DonatedLaptops from '../pages/Technician/donatedLaptops';
-import RegisterDevice from '../pages/Technician/registerDevice';
-import WrittenOffDevices from '../pages/Technician/writtenOfLaptops';
-import TechnicianProfile from '../pages/admin/register/viewTechnicians';
-import SupervisorLogin from '../pages/supervisor/Login/SupervisorLogin';
-import SupervisorDashboard from '../pages/supervisor/Dashboard/SupervisorDashboard';
-import DonationRequests from '../pages/supervisor/Dashboard/DonationRequests';
-import AddReceivedDevices from '../pages/supervisor/Dashboard/AddReceivedDevices';
-import SendDevicesToTechnician from '../pages/supervisor/Dashboard/SendDevicesToTechnician'; 
-import ViewReports from '../pages/supervisor/Dashboard/ViewReports';
+import TechnicianProfile from '../pages/admin/register/viewTechnicians'
+import StudentLogin from '../pages/student/login/studentLogin';
+
 // import other pages here as needed
 
 
@@ -41,7 +35,18 @@ const AppRoutes = () => {
       <Route path="/student/dashboard" element={<StudentDashboard />} />
       <Route path="/student/apply" element={<ApplyLaptop />} />
       <Route path="/student/track" element={<TrackApplication />} />
-      <Route path="/student/login" element={<Login />} />
+      <Route path="/student/help" element={<HelpPageLaptops />} />
+      <Route path="/student/login" element={<StudentLogin />} />
+      <Route path="/student/login/forgot-password" element={<StudentForgotPassword />} />
+      
+
+
+
+ 
+  
+
+
+      
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route path="/success" element={<SuccessPage />} />
       <Route path="/register" element={<RegisterForm />} />
@@ -49,16 +54,9 @@ const AppRoutes = () => {
       <Route path="/admin/forgot-password" element={<ForgotPassword />} />
       <Route path="/admin/students" element={<Students />} />
       <Route path="/technician/dashboard" element={<TechnicianDashboard />} />
-      <Route path="/technician/donatedLaptops" element={<DonatedLaptops />} />
-      <Route path="/technician/registerDevice" element={<RegisterDevice />} />
-      <Route path="/technician/writtenOfLaptops" element={<WrittenOffDevices />} />
       <Route path="/admin/technicians" element={<TechnicianProfile />} />
-      <Route path="/supervisor/donation-requests" element={<DonationRequests />} />
-      <Route path="/supervisor/add-received-devices" element={<AddReceivedDevices />} />
-      <Route path="/supervisor/send-devices-to-technician" element={<SendDevicesToTechnician />} />
-      <Route path="/supervisor/view-reports" element={<ViewReports />} />
-      <Route path="/supervisor/login" element={<SupervisorLogin />} />
-      <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
+   
+
 
 
       {/* Add more routes here */}
